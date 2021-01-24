7e08c068259ebba38ed2804d9fd6d35719f5dc3b
anhlh <lahonganh.k61hust@gmail.com>
2021-01-24 22:34:34 +0700

    update js

--- validateForm.js
+++ validateForm.js
@@ -3,13 +3,14 @@ function validate() {
 	var fullname = document.getElementById("fullname");
 	var lastname = document.getElementById("lastname");
 	var firstname = document.getElementById("firstname");
-	var var_phone = document.getElementById("phone");
+	var phone = document.getElementById("phone");
 	var pwd = document.getElementById("password");
 	var pwd_repeat = document.getElementById("password-repeat");
 
-	if (uname != null){
+	//check username
+	if (uname != null) {
 		if (uname.value == "" || uname.value.length < 6) {
-			if (uname.value == ""){
+			if (uname.value == "") {
 				alert("Vui lòng nhập tên đăng nhập.");
 			}
 			else {
@@ -19,9 +20,9 @@ function validate() {
 			return false;
 		}
 	}
-	
+
 	//check fullname
-	if (fullname != null){
+	if (fullname != null) {
 		if (fullname.value == "") {
 			alert("Vui lòng nhập họ tên.")
 			document.getElementById("fullname").focus();
@@ -35,41 +36,50 @@ function validate() {
 			alert("Vui lòng nhập Họ!");
 			document.getElementById("lastname").focus();
 			return false;
-		}	
-	}
-	
-	//check firstname
-	// if (firstname == "") {
-	// 	alert("Vui lòng nhập tên!");
-	// 	document.getElementById("firstname").focus();
-	// 	return false;
-	// }
-	//check password
-	if (pwd.value == "") {
-		alert("Vui lòng nhập mật khẩu!");
-		document.getElementById("password").focus();
-		return false;
-	}
-	//check password-repeat
-	if (pwd_repeat.value == "") {
-		alert("Vui lòng xác minh mật khẩu!");
-		document.getElementById("password-repeat").focus();
-		return false;
-	}
-	else if (pwd_repeat.value != pwd.value) {
-		alert("Mật khẩu không khớp. Hãy thử lại.");
-		document.getElementById("password-repeat").focus();
-		return false;
+		}
 	}
 
+	//check firstname
+	if (firstname != null) {
+		if (firstname.value == "") {
+			alert("Vui lòng nhập tên!");
+			document.getElementById("firstname").focus();
+			return false;
+		}
+
+	}
+
+	//check password
+	if (pwd != null){
+		if (pwd.value == "") {
+			alert("Vui lòng nhập mật khẩu!");
+			document.getElementById("password").focus();
+			return false;
+		}
+	}
+	
+	//check password-repeat
+	if (pwd_repeat != null) {
+		if (pwd_repeat.value == "" || pwd_repeat.value != pwd.value) {
+			if (pwd_repeat.value == "") {
+				alert("Vui lòng xác minh mật khẩu!");
+			}
+			else {
+				alert("Mật khẩu không khớp. Hãy thử lại.");
+			}
+			document.getElementById("password-repeat").focus();
+			return false;
+		}
+	}
+	
 	//check phone
-	if (var_phone.value == "") {
+	if (phone.value == "") {
 		alert("Vui lòng nhập số điện thoại!");
 		document.getElementById("phone").focus();
 		return false;
 	}
 	else {
-		var checkPhone = isNaN(var_phone.value);
+		var checkPhone = isNaN(phone.value);
 		if (checkPhone == true) {
 			alert("Điện thoại phải để ở định dạng số");
 			document.getElementById("phone").focus();
